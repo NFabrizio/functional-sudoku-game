8ea89c5ba406e821f65e6a583c0a794eb9af81b2
NFabrizio <nick.fabrizio@pearson.com>
2018-10-10 08:00:23 -0600

    Initial commit with sudoku game using underscore template

---
+++ main.js
@@ -0,0 +1,17 @@
+import { createTable } from './src/utils';
+import { renderTable, renderTimer } from './sudokuPresenter';
+
+// Check Prolog CLP(FD)
+
+let t = createTable();
+
+t[0][0] = 1;
+t[0][1] = 2
+t[1][0] = 3;
+t[1][1] = 4;
+t[2][2] = 9;
+t[2][1] = 8;
+t[1][2] = 7;
+
+renderTable(t);
+renderTimer();
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: [ 'babel-polyfill', './main.js' ],
+  output: {
+    path: __dirname,
+    filename: 'functional-sudoku-game.dist.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        presets: [ 'env' ]
+      }
+    }]
+  }
+};
